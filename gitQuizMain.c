4ad94db4d4288cf116c19ad96e91e46dd8a02aae
vvictore <116668766+vvictore@users.noreply.github.com>
2022-11-01 13:10:48 -0400

    Update gitQuizMain.c

--- gitQuizMain.c
+++ gitQuizMain.c
@@ -10,6 +10,11 @@ int main(void)
     if(val == 'y'){
         printf("You know how to use Git!");
     }
+    
+    else if(val != 'y')
+    {
+        printf("You do not know how to use Git!");
+    }
 
     printf("\n");
 
