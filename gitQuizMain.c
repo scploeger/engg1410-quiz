040f3939e7b24c4e8fe1fdad2585f9dc296158b0
Moosa Basit <moosa@CRYPTICSUPREME.com>
2022-11-01 13:08:19 -0400

    added another else statement

--- gitQuizMain.c
+++ gitQuizMain.c
@@ -10,8 +10,11 @@ int main(void)
     if(val == 'y'){
         printf("You know how to use Git!");
     }
-    else{
+    else if (val == 'n'){
         printf("You need to start working on your Git skills!");
+    }
+    else{
+        printf("Invalid answer!")
     }
 
     printf("\n");
